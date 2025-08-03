b28e244f1656a4434f26a9cacc723ff5c0fa23e3
COdyyy12 <codyforest52@gmail.com>
2025-08-04 01:17:06 +0800

    Turtle.java

--- Turtle.java
+++ Turtle.java
@@ -2,34 +2,36 @@ import java.awt.Color;
 
 public class Turtle extends Critter {
     private static final String SPECIES_NAME = "Tu";
-
     public Turtle() {
         super(SPECIES_NAME);
     }
 
+    @Override
     public Direction getMove() {
         return Direction.WEST;
     }
+    public boolean eat() {
+        String neighbor;
+        neighbor = info.getNeighbor(Direction.NORTH);
+        if (neighbor != null && neighbor.length() > 0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
+            return false;
+        }
+        neighbor = info.getNeighbor(Direction.SOUTH);
+        if (neighbor != null && neighbor.length() > 0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
+            return false;
+        }
+        neighbor = info.getNeighbor(Direction.EAST);
+        if (neighbor != null && neighbor.length() > 0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
+            return false;
+        }
+        neighbor = info.getNeighbor(Direction.WEST);
+        if (neighbor != null && neighbor.length() > 0 && !neighbor.equals(SPECIES_NAME) && !neighbor.equals(".")) {
+            return false;
+        }
+        return true;
+    }
 
-    neighbor = info.getNeighbor(Direction.NORTH);
-    if (neighbor != null && neighbor.length()>0&& !neighbor.equals(SPECIES_NAME) && !neighbor.equals(".")) {
-        return false;
-    }
-    neighbor = info.getNeighbor(Direction.SOUTH);
-    if (neighbor != null && neighbor.length()>0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
-        return false;
-    }
-
-    neighbor = info.getNeighbor(Direction.EAST);
-    if (neighbor != null && neighbor.length()>0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
-        return false;
-    }
-    
-    neighbor = info.getNeighbor(Direction.WEST);
-    if (neighbor != null && neighbor.length()>0&& !neighbor.equals(SPECIES_NAME)&& !neighbor.equals(".")) {
-        return false;
-    }
-
+    @Override
     public Attack getAttack(String opponent) {
         if (random.nextBoolean()) {
             return Attack.ROAR;
@@ -38,9 +40,11 @@ public class Turtle extends Critter {
         }
     }
 
-     public Color getColor() {
+    @Override
+    public Color getColor() {
         return Color.GREEN;
     }
 }
+
 
 
