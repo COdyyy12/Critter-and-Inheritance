d7bb9c5d72e3f21ef7d12fe1ca4a525a5bc05c76
COdyyy12 <codyforest52@gmail.com>
2025-08-04 00:32:28 +0800

    Feline.java

--- Feline.java
+++ Feline.java
@@ -1,26 +1,33 @@
 import java.awt.Color;
 
 public class Feline extends Critter {
-    private int moveCount; // counter for getMove method before random direction
-    private int eatCount; // counter for eating
+    private int moveCount;   // counter for getMove before changing direction
+    private int eatCount;    // counter for eating
     private Direction currDir; // current direction
-
+    
     public Feline() {
         super("Fe");
-        moveCount = 5;       
+        moveCount = 5;   
         eatCount = 0;
-        currDir = Direction.CENTER;
+        currDir = Direction.NORTH; 
     }
+    
     @Override
     public Direction getMove() {
         if (moveCount >= 5) {
-            Direction[] options = {Direction.NORTH, Direction.SOUTH,Direction.EAST,Direction.WEST};
+            Direction[] options = {
+                Direction.NORTH,
+                Direction.SOUTH,
+                Direction.EAST,
+                Direction.WEST
+            };
             currDir = options[random.nextInt(options.length)];
             moveCount = 0;
         }
         moveCount++;
         return currDir;
     }
+
     @Override
     public boolean eat() {
         eatCount++;
@@ -30,11 +37,10 @@ public class Feline extends Critter {
         }
         return false;
     }
+
+    @Override
     public Attack getAttack(String opponent) {
         return Attack.POUNCE;
     }
-    @Override
-    public Color getColor() {
-        return Color.MAGENTA;
-    }
 }
+
